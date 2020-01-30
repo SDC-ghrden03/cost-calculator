c5e63860598b6e6a493774b3bb90ba4ea876cdc0
Shannon Hurley <shannonhurley@Shannons-MacBook-Air.local>
2020-01-29 19:33:54 -0700

    zipcode works and updated some frontend to not be forms

--- client/src/component/Form.jsx
+++ client/src/component/Form.jsx
@@ -12,12 +12,36 @@ class Form extends React.Component {
       displayedRate: 0,
       creditScore: 1,
       term: 24,
+      zipcode: 'Zip Code',
       myMonthlyPayment: 0
     }
     this.setMonthlyPayment = this.setMonthlyPayment.bind(this);
     this.onFormChange = this.onFormChange.bind(this);
     this.onFormSubmit = this.onFormSubmit.bind(this);
     this.changeRateEstimate = this.changeRateEstimate.bind(this);
+    this.enterZipcode = this.enterZipcode.bind(this);
+    this.hideTaxesAndFees = this.hideTaxesAndFees.bind(this);
+    this.hideAll = this.hideAll.bind(this);
+    this.showTaxesAndFees = this.showTaxesAndFees.bind(this);
+    this.showCalculateTaxesAndFees = this.showCalculateTaxesAndFees.bind(this);
+  }
+  hideAll () {
+    document.getElementById("calculateTaxesAndFees").style.display="none"
+    document.getElementById("taxesAndFees").style.display="none"
+    document.getElementById("calculateButton").style.display="block"
+  }
+  hideTaxesAndFees () {
+    document.getElementById("calculateTaxesAndFees").style.display="block"
+    document.getElementById("taxesAndFees").style.display="none"
+    document.getElementById("calculateButton").style.display="none"
+  }
+  showCalculateTaxesAndFees() {
+    document.getElementById("calculateTaxesAndFees").style.display="block";
+    document.getElementById("calculateButton").style.display="none"
+  }
+  showTaxesAndFees() {
+    document.getElementById("taxesAndFees").style.display="block";
+    document.getElementById("calculateButton").style.display="none"
   }
   changeRateEstimate (event) {
     const key = 'displayedRate';
@@ -32,7 +56,7 @@ class Form extends React.Component {
     let r = this.state.rateEstimate * this.state.creditScore * .01 / 12
     let a = this.state.cost - this.state.cashDown - (this.state.tradeInValue - this.state.owed)
     let d = (Math.pow(1+r, n) - 1) / (r * Math.pow(1+r, n))
-    let payment = a / d
+    let payment = Math.round(a / d)
     this.setState({
       myMonthlyPayment: payment
     })
@@ -41,6 +65,7 @@ class Form extends React.Component {
     // Typical usage (don't forget to compare props):
     if (this.props.state.cost !== prevProps.state.cost) {
       this.setState({cost:this.props.state.cost});
+      this.setMonthlyPayment()
     }
   }
   onFormChange (event) {
@@ -51,12 +76,22 @@ class Form extends React.Component {
       [key]:value
     })
   }
+  enterZipcode(event) {
+    event.preventDefault();
+    let zipcode = document.getElementById("zipcode").value;
+    console.log(zipcode)
+    this.props.getZipcode(zipcode)
+  }
+  clearZipcode() {
+
+  }
   onFormSubmit(event) {
     event.preventDefault();
     this.setMonthlyPayment();
   }
   render() {
     return (
+      <div>
       <form onSubmit={this.onFormSubmit}>
       <label> Cash Down
         <input name="cashDown" value={this.state.cashDown} onChange={this.onFormChange} />
@@ -89,11 +124,30 @@ class Form extends React.Component {
       <option value={84}>84</option>
         </select>
       </label>
-      <label> My Monthly Payment
-        <input name="myMonthlyPayment" value={this.state.myMonthlyPayment} readOnly/>
-      </label>
       <button>Test</button>
       </form>
+      <button id="calculateButton" onClick={this.showCalculateTaxesAndFees}>Calculate My Taxes and Fees</button>
+      <form id="calculateTaxesAndFees">
+        <label> Where Will This Vehicle Be Registered?
+          <input id="zipcode" name="zipcode" value={this.state.zipcode} onChange={this.onFormChange} />
+        </label>
+        <button onClick={this.hideAll}>Back</button><button onClick={this.enterZipcode} onClick={this.showTaxesAndFees}>Next</button>
+      </form>
+      <div id="taxesAndFees">
+      <label> 
+        Taxes ${this.props.state.taxes}
+      </label>
+      <br></br>
+      <label> 
+        DMV Fees ${this.props.state.fees}
+      </label>
+      <br></br>
+      <button onClick={this.hideAll}>Remove</button><button onClick={this.hideTaxesAndFees}>Edit</button>
+      </div>
+      <label> 
+      My Monthly Payment             ${this.state.myMonthlyPayment}/mo  
+      </label>
+      </div>
     )
   }
 }
