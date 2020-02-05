cd535387c242ce8cbcd2bd078b68459cf5c5908b
Shannon Hurley <shannonhurley@Shannons-MacBook-Air.local>
2020-02-05 15:11:55 -0700

    red functionality with error on inputs working, need to add more if statements to each to make them all work independently

--- client/src/__tests__/App.test.js
+++ client/src/__tests__/App.test.js
@@ -1,17 +1,36 @@
 import React from 'react';
-import {shallow} from "enzyme";
+import {mount, shallow, render} from "enzyme";
 //const App = require('../component/App.jsx');
 import {App} from '../component/App.jsx'
-import renderer from "react-test-renderer";
+import ReactDOM from 'react-dom';
 
-describe('App', ()=>{
-  let wrapper = renderer.create(<App />)
-  window.fetch = jest.fn();
-  // beforeEach(()=>{
-  //   wrapper = shallow(<App />)
-  // })
-  let component = wrapper.toJSON()
-  it('should match snapshot', ()=>{
-    expect(component).toMatchSnapshot();
+describe('Testing App', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = mount(<App
+      cost="1000"
+      />
+    )
+  })
+  it('Renders without crashing', () => {
+    expect(wrapper).toBeTruthy();
+  })
+  it('Recieves Props', () => {
+    expect(wrapper.props(name)).toBeTruthy();
+    console.log("Here are the props:", wrapper.props())
+    expect(wrapper.props().cost).toBe('1000');
   })
 })
+
+
+// describe('App', ()=>{
+//   let wrapper = renderer.create(<App />)
+//   window.fetch = jest.fn();
+//   // beforeEach(()=>{
+//   //   wrapper = shallow(<App />)
+//   // })
+//   let component = wrapper.toJSON()
+//   it('should match snapshot', ()=>{
+//     expect(component).toMatchSnapshot();
+//   })
+// })
--- client/src/component/Form.jsx
+++ client/src/component/Form.jsx
@@ -9,6 +9,7 @@ class Form extends React.Component {
       tradeInValue: 0,
       owed: 0,
       rateEstimate: 3.49,
+      ownRate: 3.49,
       displayedRate: 3.49,
       creditScore: 1,
       maxCreditScore: 820,
@@ -20,7 +21,7 @@ class Form extends React.Component {
     this.onFormChange = this.onFormChange.bind(this);
     this.onCreditChange = this.onCreditChange.bind(this);
     this.onFormSubmit = this.onFormSubmit.bind(this);
-    this.changeRateEstimate = this.changeRateEstimate.bind(this);
+    // this.changeRateEstimate = this.changeRateEstimate.bind(this);
     this.enterZipcode = this.enterZipcode.bind(this);
     this.hideTaxesAndFees = this.hideTaxesAndFees.bind(this);
     this.hideAll = this.hideAll.bind(this);
@@ -44,14 +45,22 @@ class Form extends React.Component {
     this.myMonthlyPaymentMouseout = this.myMonthlyPaymentMouseout.bind(this);
     this.hideInputError = this.hideInputError.bind(this);
     this.showInputError = this.showInputError.bind(this);
+    this.hideLoan = this.hideLoan.bind(this);
+    this.showLoan = this.showLoan.bind(this);
+    this.hideOwnRate = this.hideOwnRate.bind(this);
+    this.showOwnRate = this.showOwnRate.bind(this);
   }
   componentDidUpdate(prevProps) {
     // Typical usage (don't forget to compare props):
     if (this.props.state.cost !== prevProps.state.cost) {
       this.setState({cost:this.props.state.cost});
       this.setState({
-        displayedRate: this.state.rateEstimate * this.state.creditScore,
         cashDown: this.props.state.cost * .1
+      }, ()=>{this.setMonthlyPayment()})
+    }
+    if (this.props.state.rate !== prevProps.state.rate) {
+      this.setState({displayedRate:this.props.state.rate * this.state.creditScore,
+        rateEstimate:this.props.state.rate
       }, ()=>{this.setMonthlyPayment()})
     }
   }
@@ -66,6 +75,8 @@ class Form extends React.Component {
     this.taxesAndFeesMouseout();
     this.myMonthlyPaymentMouseout();
     this.hideInputError();
+    this.showLoan();
+    this.hideOwnRate();
   }
   
   hideAll () {
@@ -101,22 +112,45 @@ class Form extends React.Component {
   showInputError() {
     document.getElementById("inputError").style.display="block"
   }
-  changeRateEstimate (event) {
-    const key = 'displayedRate';
-    const value = event.target.value[0] * this.state.rateEstimate;
-    this.setState({
-      [key]:value,
-      maxCreditScore: event.target.value[1]
-    }, ()=>{console.log('rateEstimate', this.state.rateEstimate)
-    console.log('displayedRate', this.state.displayedRate)})
+  hideLoan() {
+    document.getElementById("loan").style.display="none"
+    document.getElementById("cash").style.display="block"
+    document.getElementById("container").style.height="125px"
   }
+  showLoan() {
+    document.getElementById("loan").style.display="block"
+    document.getElementById("cash").style.display="none"
+    document.getElementById("container").style.height="525px"
+  }
+  hideOwnRate() {
+    document.getElementById("rate").style.display="block"
+    document.getElementById("ownRate").style.display="none"
+  }
+  showOwnRate(event) {
+    event.preventDefault()
+    document.getElementById("ownRate").style.display="block"
+    document.getElementById("rate").style.display="none"
+  }
+  // changeRateEstimate (event) {
+  //   const key = 'displayedRate';
+  //   const value = this.state.creditScore * this.state.rateEstimate;
+  //   this.setState({
+  //     [key]:value,
+  //     maxCreditScore: event.target.value[1]
+  //   }, ()=>{console.log('rateEstimate', this.state.rateEstimate)
+  //   console.log('displayedRate', this.state.displayedRate)})
+  // }
   setMonthlyPayment () {
     const carCost = Number(this.props.state.cost)
     const cash = Number(this.state.cashDown)
     const trade = Number(this.state.tradeInValue)
     const owe = Number(this.state.owed)
     const term = Number(this.state.term)
-    const rate = Number(this.state.rateEstimate)
+    if(document.getElementById("rate").style.display === "block") {
+    var rate = Number(this.state.rateEstimate)
+    } else if (document.getElementById("rate").style.display==="none") {
+      var rate = Number(this.state.ownRate)
+    }
     const credit = Number(this.state.creditScore)
     if (cash > carCost || trade > carCost || owe > 100000 || (cash + trade - owe) > carCost) {
       this.showInputError()
@@ -145,14 +179,22 @@ class Form extends React.Component {
     }
   }
   onCreditChange (event) {
-    const value1 = event.target.value.split(',')[0] * this.state.rateEstimate;
+    let value1 = event.target.value.split(',')[0];
     const value2 = event.target.value.split(',')[1];
+    if(document.getElementById("ownRate").style.display==="block") {
+      var displayed = this.state.ownRate
+      value1 = 1;
+    } else if (document.getElementById("ownRate").style.display==="none") {
+      var displayed = (value1 * this.state.rateEstimate).toFixed(2)
+    }
+    
     console.log('value1', value1)
     console.log('value2', value2)
     this.setState({
       creditScore: value1,
-      maxCreditScore: value2
-    })
+      maxCreditScore: value2,
+      displayedRate: displayed
+    }, ()=>{this.setMonthlyPayment()})
   }
   onFormChange (event) {
     const key = event.target.name;
@@ -236,58 +278,107 @@ class Form extends React.Component {
   }
   render() {
     return (
-      <div>
+      <div id="container">
+      <div id="loan">
+      <h3>What's It Going To Cost?</h3>
       <form id="costCalculator" onSubmit={this.onFormSubmit}>
-      <label> Cash Down
-        <input type="number" name="cashDown" value={this.state.cashDown} onChange={this.onFormChange} />
-        <div onMouseOver={this.cashDownMouseover} onMouseOut={this.cashDownMouseout}>?</div>
-        <div id="cashDownHoverText">
-        <label> Cash Down</label>
-        Cash down may include Security Deposit, First Month Payment, and any non-capitalized fees. The remaining amount reduces the capitalized cost of the vehicle for leased vehicles or the amount financed.
-        </div>
-      </label>
-      <label> My Trade-In Value
-        <input type="number" name="tradeInValue" value={this.state.tradeInValue} onChange={this.onFormChange}/>
-        <div onMouseOver={this.tradeInValueMouseover} onMouseOut={this.tradeInValueMouseout}>?</div>
-        <div id="tradeInValueHoverText">
-        <label> Trade-In Value </label>
+      <div className="row">
+
+      <label className="titles"> Cash Down</label>
+      <input className="inputMoney" value="$" readOnly/>
+        <input className="inputs formInput" type="number" name="cashDown" value={this.state.cashDown} onChange={this.onFormChange} />
+        <div className="helpHolder">
+        <div className="help" onMouseOver={this.cashDownMouseover} onMouseOut={this.cashDownMouseout}>?
+      <div className="helperText" id="cashDownHoverText">
+      
+      <h3 className="helperTitle"> Cash Down</h3>
+      Cash down may include Security Deposit, First Month Payment, and any non-capitalized fees. The remaining amount reduces the capitalized cost of the vehicle for leased vehicles or the amount financed.
+      </div>
+      </div>
+      </div>
+      </div>
+      <br></br>
+      <div className="row">
+      <label className="titles tradeIn"> My Trade-In Value &nbsp;
+      <i className="fas fa-calculator" style={{color:'green'}}>&nbsp;</i>
+       </label>
+        <input className="inputMoney" value="$" readOnly/>
+      <input className="inputs formInput" type="number" name="tradeInValue" value={this.state.tradeInValue} onChange={this.onFormChange}/>
+      <div className="helpHolder">
+      <div className="help" onMouseOver={this.tradeInValueMouseover} onMouseOut={this.tradeInValueMouseout}>?
+        <div className="helperText" id="tradeInValueHoverText">
+        <h3 className="helperTitle"> Trade-In Value </h3>
         The value of your car. If you don't plan on trading in a vehicle this can be left as zero.
         </div>
-      </label>
-      <label> Owed On Trade
-        <input type="number" name="owed" value={this.state.owed} onChange={this.onFormChange}/>
-        <div onMouseOver={this.owedOnTradeMouseover} onMouseOut={this.owedOnTradeMouseout}>?</div>
-        <div id="owedOnTradeHoverText">
-        <label> Owed On Trade </label>
-        The amount you owe on the car. If you don't plan on trading in a vehicle this can be left as zero.
+        </div>   
         </div>
-      </label>
-      <label> Rate Estimated By Echo Park:
-        <input name="rateEstimate" value={this.state.displayedRate} readOnly/>
-        <div onMouseOver={this.rateEstimateMouseover} onMouseOut={this.rateEstimateMouseout}>?</div>
-        <div id="rateEstimateHoverText">
-        <label> Standard Program </label>
+      </div>
+      <br></br>
+      <div className="row">
+      <label className="titles"> Owed On Trade</label>
+      
+      <input className="inputMoney" value="$" readOnly/>
+        <input className="inputs formInput" type="number" name="owed" value={this.state.owed} onChange={this.onFormChange}/>
+        <div className="helpHolder">
+        <div className="help" onMouseOver={this.owedOnTradeMouseover} onMouseOut={this.owedOnTradeMouseout}>?
+      <div className="helperText" id="owedOnTradeHoverText">
+      <h3 className="helperTitle"> Owed On Trade </h3>
+      The amount you owe on the car. If you don't plan on trading in a vehicle this can be left as zero.
+      </div>
+      </div>
+      </div>
+      </div>
+      <br></br>
+      <div className="row" id="rate">
+      
+      <label className="titles"> Rate Estimated By Echo Park:</label>
+        <input className="inputs percent" name="rateEstimate" value={this.state.displayedRate} readOnly/>
+        <input className="inputPercent" value="%" readOnly/>
+        <div className="helpHolder">
+        <div className="help" onMouseOver={this.rateEstimateMouseover} onMouseOut={this.rateEstimateMouseout}>?
+        <div className="helperText" id="rateEstimateHoverText">
+        <h3 className="helperTitle"> Standard Program </h3>
         The lowest available monthy payment financing program offered for this vehicle based on rates from participating lenders in your area. This rate is for well qualified buyers only, actual rates may vary.
         </div>
-      </label>
+        </div> 
+        </div>
+        <br></br>
+        <button onClick={this.showOwnRate}>Enter Own Rate</button>
+      </div>
+      <div className="row" id="ownRate">
+      <label className="titles"> Enter Own Rate:</label>
+        <input className="inputs percent" name="ownRate" value={this.state.ownRate} onChange={this.onFormChange}/>
+        <input className="inputPercent" value="%" readOnly/>
+        <br></br>
+        <button onClick={this.hideOwnRate}>Use estimated Rate</button>
+      </div>
+      <br></br>
       <div id="inputError">
       No lender found, please enter your own rate
+      <br></br>
       </div>
-      <label> Est. Credit Score
-      <select name="creditScore" id="creditScore" onChange={this.onCreditChange} >
+      <div className="row">
+      
+      <label className="titles"> Est. Credit Score</label>
+      <select className="inputs" name="creditScore" id="creditScore" onChange={this.onCreditChange} >
       <option value="1,820">740-900</option>
       <option value="1.35,720">700-739</option>
       <option value="1.5,685">670-699</option>
       <option value="2,650">630-669</option>
       <option value="2.85,605">580-629</option>
         </select>
-        <div onMouseOver={this.creditScoreMouseover} onMouseOut={this.creditScoreMouseout}>?</div>
-        <div id="creditScoreHoverText">
-        <label> Est. Credit Score </label>
+        <div className="helpHolder">
+        <div className="help" onMouseOver={this.creditScoreMouseover} onMouseOut={this.creditScoreMouseout}>?
+        <div className="helperText" id="creditScoreHoverText">
+        <h3 className="helperTitle"> Est. Credit Score </h3>
         Your credit score is an approximate estimation of your credit worthiness and will impact your estimated monthly payment. Please enter a FICO credit score between 580 and 900.
         </div>
-      </label>
-      <label> Term
+        </div>
+        </div>
+      </div>
+      <br></br>
+      <div className="row">
+      <label className="titles" id="termCss"> Term</label>
       <div className="radio-term" onChange={this.onFormChange}>
     <input type="radio" id="24" value={24} name="term"></input>
     <label htmlFor="24">24</label>
@@ -302,21 +393,29 @@ class Form extends React.Component {
     <input type="radio" id="84" value={84} name="term"></input>
     <label htmlFor="84">84</label>
       </div>
-      <div onMouseOver={this.termMouseover} onMouseOut={this.termMouseout}>?</div>
-        <div id="termHoverText">
-        <label> Term </label>
+      <div className="helpHolder">
+      <div className="help" onMouseOver={this.termMouseover} onMouseOut={this.termMouseout}>?
+        <div className="helperText" id="termHoverText">
+        <h3 className="helperTitle"> Term </h3>
         The number of months over which payments will be payed.
         </div>
-      </label>
+        </div>
+        </div>
+      </div>
+      <br></br>
       </form>
-      <label>Taxes and Fees
-      <div onMouseOver={this.taxesAndFeesMouseover} onMouseOut={this.taxesAndFeesMouseout}>?</div>
-        <div id="taxesAndFeesHoverText">
-        <label> General Disclaimer </label>
+      <div>
+      <label className="titles" id="taxAndFeesCss">Taxes and Fees</label>
+      <div className="helpHolder">
+      <div className="help" onMouseOver={this.taxesAndFeesMouseover} onMouseOut={this.taxesAndFeesMouseout}>?
+        <div className="helperText" id="taxesAndFeesHoverText">
+        <h3 className="helperTitle"> General Disclaimer </h3>
         Taxes are an estimate based on the price of the vehicle as shown above and do not include additional taxes on additional product add-ons and services purchased at the dealership.
         </div>
-      </label>
-      <button id="calculateButton" onClick={this.showCalculateTaxesAndFees}>Calculate My Taxes and Fees</button>
+        </div>
+        </div>
+        </div>
+      <button id="calculateButton" onClick={this.showCalculateTaxesAndFees}>Calculate My Taxes and Fees &nbsp; <i className="fas fa-calculator" style={{color:'green'}}></i></button>
       <form id="calculateTaxesAndFees">
         <label> Where Will This Vehicle Be Registered?
           <input id="zipcode" name="zipcode" value={this.state.zipcode} onChange={this.onFormChange} />
@@ -335,15 +434,37 @@ class Form extends React.Component {
       <br></br>
       <button onClick={this.hideAll}>Remove</button>
       <button onClick={this.hideTaxesAndFees}>Edit</button>
+      <br></br>
       </div>
+      <br></br>
       <label> 
-      My Monthly Payment             ${this.state.myMonthlyPayment}/mo  
-      <div onMouseOver={this.myMonthlyPaymentMouseover} onMouseOut={this.myMonthlyPaymentMouseout}>?</div>
-      <div id="myMonthlyPaymentHoverText">
-      <label> My Monthly Payment </label>
+      My Monthly Payment             <b>${this.state.myMonthlyPayment}</b>/mo  
+      <div className="helpHolder">
+      <div className="help" onMouseOver={this.myMonthlyPaymentMouseover} onMouseOut={this.myMonthlyPaymentMouseout}>?
+      <div className="helperText" id="myMonthlyPaymentHoverText">
+      <h3 className="helperTitle"> My Monthly Payment </h3>
       Monthly payment amount applies to qualified credit or lease applicants having minimum credit score of {this.state.maxCreditScore}. Taxes, title, and registration fees are additional. Your monthly payment is established based on a full review of your credit application and credit report.
       </div>
+      </div>
+      </div>
       </label>
+      <br></br>
+      <div className="cashOnly">
+      or, &nbsp;
+      <button onClick={this.hideLoan}>Plan to Pay Cash in Full</button>
+      </div>
+      </div>
+      <div id="cash">
+      <label>What's It Going to Cost?</label>
+      <button onClick={this.showLoan}>Edit</button>
+      <br></br>
+      My Payments
+      <br></br>
+        $0
+        <br></br>
+      Plan to Pay Cash in Full
+      </div>
+      
       </div>
     )
   }
@@ -351,12 +472,3 @@ class Form extends React.Component {
 
 export default Form;
 
-// <select name = "term" onChange={this.onFormChange}>
-//       <option value={24}>24</option>
-//       <option value={36}>36</option>
-//       <option value={48}>48</option>
-//       <option value={60}>60</option>
-//       <option value={72}>72</option>
-//       <option value={84}>84</option>
-//         </select>
-// onChange={this.changeRateEstimate}
