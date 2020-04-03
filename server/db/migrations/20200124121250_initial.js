f44bac87d80455178e94241967855889a6355433
Justin Hubbell <hubbsy@Justins-MacBook-Pro.local>
2020-04-02 22:23:23 -0600

    successfully created Knex connection and schema via knex

--- server/db/migrations/20200124121250_initial.js
+++ server/db/migrations/20200124121250_initial.js
@@ -1,19 +1,15 @@
-exports.up = function(knex, Promise) {
-    return Promise.all([
-        knex.schema.createTable('location', function (table) {
+exports.up = async function(knex, Promise) {
+
+
+        await knex.schema.createTable('location', function (table) {
            table.increments('id').primary();
            table.integer('zipcode').notNullable();
            table.decimal('creditscore').notNullable();
-        }),
-        knex.schema.createTable('cars', function (table) {
-           table.increments('id').primary();
-           table.integer('price').notNullable();
-        })
-    ]);
+        })   
+        
 };
 
-exports.down = function(knex, Promise) {
-    return knex.schema
-    .dropTable("location")
-    .dropTable("cars");
+
+exports.down =  async function(knex, Promise) {
+    await knex.schema.dropTable("location")    
 };
